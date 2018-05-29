97ce3ec292836c49014ca529717463a782b051a6
Edmund Miller <eman0088@gmail.com>
2018-05-29 10:35:59 -0500

    Change to eshell/open-popup

--- config.el
+++ config.el
@@ -18,7 +18,7 @@
  (:leader
    (:prefix "o"
      :desc "Agenda" :n "a" #'org-agenda
-     :desc "eShell" :n "e" #'eshell
+     :desc "eShell" :n "e" #'+eshell/open-popup
      :desc "i.org" :n "o" #'emiller/visit-i-org
      :desc "APP: Email" :n "m" #'=mail)
    (:prefix "p"
