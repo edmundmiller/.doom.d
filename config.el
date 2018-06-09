a4e3e0c23880d499d173806bc08ef6933b121804
Edmund Miller <eman0088@gmail.com>
2018-06-08 19:55:40 -0500

    Remove mc-evil cursor
    Henrik added it manually.

--- config.el
+++ config.el
@@ -58,12 +58,6 @@
  ;;   :n "M-j" #'org-metadown
  ;;   :n "M-k" #'org-metaup))
 
-(after! evil-mc
-  ;; Make evil-mc resume its cursors when I switch to insert mode
-  (add-hook! 'evil-mc-before-cursors-created
-    (add-hook 'evil-insert-state-entry-hook #'evil-mc-resume-cursors nil t))
-  (add-hook! 'evil-mc-after-cursors-deleted
-(remove-hook 'evil-insert-state-entry-hook #'evil-mc-resume-cursors t)))
 (after! org
   (setq org-directory "~/Dropbox/orgfiles")
 
