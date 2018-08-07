ac98a7000085165b9f221eb8a056eed6c59a5263
Edmund Miller <edmund.a.miller@protonmail.com>
2018-08-07 08:52:59 -0500

    Fix Formatting and add langtool

--- init.el
+++ init.el
@@ -133,13 +133,13 @@
        ;; should be loaded late.
        :app
         mail
-        ;+gmail)          ; emacs as an email client
-        ;irc              ; how neckbeards socialize
+        ;+gmail)         ; emacs as an email client
+        ;irc             ; how neckbeards socialize
         (rss +org)       ; emacs as an RSS reader
-        ;twitter          ; twitter client https://twitter.com/vnought
-        (write            ; emacs as a word processor (latex + org + markdown)
-        +wordnut)         ; wordnet (wn) search
-        ;+langtool)       ; a proofreader (grammar/style check) for Emacs
+        ;twitter         ; twitter client https://twitter.com/vnought
+        (write           ; emacs as a word processor (latex + org + markdown)
+        +wordnut         ; wordnet (wn) search
+        +langtool)       ; a proofreader (grammar/style check) for Emacs
 
         :collab
          floobits          ; peer programming for a price
