273cd50b22993f81524f2b0c36c34ac74e8aae9e
Edmund Miller <edmund.a.miller@protonmail.com>
2019-10-20 16:12:31 -0500

    fix: update packages

--- packages.el
+++ packages.el
@@ -7,7 +7,7 @@
 (package! org-noter)
 (package! org-clock-csv)
 (package! easy-hugo)
-(package! package-lint)
+;; (package! package-lint)
 (package! graphviz-dot-mode)
 (package! jest)
 (package! vyper-mode)
@@ -15,10 +15,8 @@
 (package! dired-sidebar)
 ;; Experimental
 (package! reformatter)
-(package! org-super-agenda)
 (package! tox)
 (package! emms)
-(package! hydra-posframe :recipe (:fetcher github :repo "ladicle/hydra-posframe" :files ("*")))
 (package! github-review)
 
 (provide 'packages)
