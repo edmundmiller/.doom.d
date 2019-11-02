1146e37760bd121c741223bc1bb58f78471c751c
Edmund Miller <edmund.a.miller@protonmail.com>
2019-11-01 22:48:40 -0500

    fix(snakemake): remove after python

--- modules/lang/snakemake/config.el
+++ modules/lang/snakemake/config.el
@@ -1,11 +1,10 @@
 ;;; lang/snakemake/config.el -*- lexical-binding: t; -*-
 
 ;; Snakemake
-(after! python
-  (use-package! snakemake-mode
-    :config
-    (map!)
-    (:leader
-      (:prefix "c"
-        :desc "Snakemake" :n "s" #'snakemake-popup
-        :desc "Snakemake Graph" :n "g" #'snakemake-graph-this-file))))
+(use-package! snakemake-mode
+  :config
+  (map!
+   (:leader
+    (:prefix "c"
+      :desc "Snakemake" :n "s" #'snakemake-popup
+      :desc "Snakemake Graph" :n "g" #'snakemake-graph-this-file))))
