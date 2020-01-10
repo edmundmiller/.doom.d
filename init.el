46f5225dd37ecb2f196e1274c85d4f015cf6fade
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-10 13:53:26 -0600

    fix: init typo because of parinfer

--- init.el
+++ init.el
@@ -65,8 +65,6 @@
        editorconfig
        ;; ein
        eval
-       flycheck
-       flyspell
        gist
        (lookup
         +docsets)
@@ -152,13 +150,13 @@
 
        :config
        (default +bindings +smartparnes)
-       literate
-       (custom-set-variables)
-       ;; custom-set-variables was added by Custom.
-       ;; If you edit it by hand, you could mess it up, so be careful.
-       ;; Your init file should contain only one such instance.
-       ;; If there is more than one, they won't work right.
-       '(safe-local-variable-values
-         '((ssh-deploy-async-with-threads . 1)
-           (ssh-deploy-on-explicity-save . t)
-           (ssh-deploy-async . 1))))
+       literate)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((ssh-deploy-async-with-threads . 1)
+     (ssh-deploy-on-explicity-save . t)
+     (ssh-deploy-async . 1))))
