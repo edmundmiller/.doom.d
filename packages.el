70e7bc9c5043e287a7cee487a58df1c25725ec30
Edmund Miller <edmund.a.miller@protonmail.com>
2020-01-17 00:11:45 -0600

    feat: rewrite org captures with doct

--- packages.el
+++ packages.el
@@ -13,6 +13,7 @@
 (package! vyper-mode)
 (package! org-gcal)
 (package! dired-sidebar)
+(package! doct :recipe (:host github :repo "progfolio/doct"))
 ;; Experimental
 (package! tox)
 (package! emms)
