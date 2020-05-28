580e361729de239440ae3cb392437953e09a769a
Edmund Miller <edmund.a.miller@protonmail.com>
2020-05-28 17:09:17 -0500

    feat(eglot): init and add python support

--- init.el
+++ init.el
@@ -77,7 +77,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        (lookup +docsets +dictionary)              ; navigate your code and its documentation
-       lsp
+       (lsp +eglot)
        ;;macos             ; MacOS-specific commands
        (magit +forge)             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
