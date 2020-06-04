05a888b55714aa439acdae2e83fbbdc6e1a3c85a
Edmund Miller <edmund.a.miller@protonmail.com>
2020-06-04 08:19:40 -0500

    fix: remove company childframe
    Trying to get rid of the lag on my laptop

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
        ;;japanese
 
        :completion
-       (company +childframe)           ; the ultimate code completion backend
+       company             ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ivy               ; a search engine for love and life
