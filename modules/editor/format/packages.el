e70bfc5d4b5d3d116ab3a1c9eff0fa05e45d5ad0
Edmund Miller <edmund.a.miller@protonmail.com>
2020-07-11 21:20:30 -0500

    Remove new format module
    It's flaky for me

--- modules/editor/format/packages.el
+++
@@ -1,6 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; editor/format/packages.el
-
-(package! apheleia
-  :recipe (:host github :repo "raxod502/apheleia")
-  :pin "3e342632b834a78f31ead48b94392e00dba1542c")
