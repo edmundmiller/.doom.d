ab72e4ecf3d3691bb206c82aea2136ec2cb83129
Edmund Miller <edmund.a.miller@protonmail.com>
2020-08-27 09:17:14 -0500

    feat(functions): add lower-case-org-keywords

--- autoload/emiller.el
+++ autoload/emiller.el
@@ -45,3 +45,30 @@
   "Archive all done tasks."
   (interactive)
   (org-map-entries 'org-archive-subtree "/DONE" 'file))
+
+;;;###autoload
+(defun modi/lower-case-org-keywords ()
+  "Lower case Org keywords and block identifiers.
+
+Example: \"#+TITLE\" -> \"#+title\"
+         \"#+BEGIN_EXAMPLE\" -> \"#+begin_example\"
+
+Inspiration:
+https://code.orgmode.org/bzg/org-mode/commit/13424336a6f30c50952d291e7a82906c1210daf0."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((case-fold-search nil)
+          (count 0))
+      ;; Match examples: "#+FOO bar", "#+FOO:", "=#+FOO=", "~#+FOO~",
+      ;;                 "‘#+FOO’", "“#+FOO”", ",#+FOO bar",
+      ;;                 "#+FOO_bar<eol>", "#+FOO<eol>".
+      (while (re-search-forward "\\(?1:#\\+[A-Z_]+\\(?:_[[:alpha:]]+\\)*\\)\\(?:[ :=~’”]\\|$\\)" nil :noerror)
+        (setq count (1+ count))
+        (replace-match (downcase (match-string-no-properties 1)) :fixedcase nil nil 1))
+      (message "Lower-cased %d matches" count))))
+
+(dolist (f (org-roam--list-all-files))
+  (with-current-buffer (find-file f)
+    (modi/lower-case-org-keywords)
+    (save-buffer)))
