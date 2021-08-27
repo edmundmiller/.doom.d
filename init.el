09227ba2cc30011483cc5865cf837f9a98272a54
Edmund Miller <edmund.a.miller@protonmail.com>
2021-08-26 21:59:23 -0500

    feat: Use everywhere

--- init.el
+++ init.el
@@ -173,6 +173,7 @@
 
        :app
        calendar
+       everywhere
        irc               ; how neckbeards socialize
        (rss +org)        ; emacs as an RSS reader
        twitter           ; twitter client https://twitter.com/vnought
