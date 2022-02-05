da7637420446b3fdec5487192d5320f3c1f42f19
Edmund Miller <edmund.a.miller@protonmail.com>
2022-02-05 14:34:10 -0600

    feat: Add beancount with lsp

--- init.el
+++ init.el
@@ -97,6 +97,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;assembly          ; assembly for fun or debugging
+       (beancount +lsp)    ; mind the GAAP
        ;;cc                ; C/C++/Obj-C madness
        clojure           ; java with a lisp
        common-lisp       ; if you've seen one lisp, you've seen them all
