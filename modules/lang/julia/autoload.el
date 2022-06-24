65445c898e93ad61978dc5dcc0661c4df9822524
Edmund Miller <edmund.a.miller@protonmail.com>
2022-06-24 15:17:17 -0500

    feat(Julia): Update REPL to use snail

--- modules/lang/julia/autoload.el
+++ modules/lang/julia/autoload.el
@@ -9,13 +9,5 @@
 (defun +julia/open-repl ()
   "Run an inferior instance of `julia' inside Emacs."
   (interactive)
-  (if (require 'julia-repl nil t)
-      (prog1 (julia-repl)
-        (julia-repl-use-emacsclient))
-    (let ((buffer (get-buffer-create "*Julia*")))
-      (unless (comint-check-proc "*Julia*")
-        (apply #'make-comint-in-buffer "Julia" "*Julia*" julia-program julia-arguments))
-      (pop-to-buffer buffer)
-      (with-current-buffer buffer
-        (inferior-julia-mode))
-      buffer)))
+  (julia-snail)
+  (current-buffer))
