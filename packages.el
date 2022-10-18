d2fe50546310e1e5d4803f426b3816085f4cbcc1
Edmund Miller <edmund.a.miller@protonmail.com>
2022-10-17 22:07:40 -0500

    chore: Move back to local-repo for nextflow

--- packages.el
+++ packages.el
@@ -7,7 +7,7 @@
 ;; (package! package-lint)
 (package! graphviz-dot-mode)
 (package! jest :recipe (:local-repo "~/src/emacs/emacs-jest" :build (:not compile)))
-(package! nextflow-mode :recipe (:host github :repo "jacksondm33/nextflow-mode" :build (:not compile)))
+(package! nextflow-mode :recipe (:local-repo "~/src/emacs/nextflow-mode" :build (:not compile)))
 (package! academic-phrases)
 (package! mu4e-conversation)
 (package! mu4e-patch :recipe (:host github :repo "seanfarley/mu4e-patch"))
