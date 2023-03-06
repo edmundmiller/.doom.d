2b844205896ebf76eabb562f9f27162419f67364
Edmund Miller <edmund.a.miller@protonmail.com>
2023-03-06 15:11:04 -0600

    fix: Remove old elfeed-tube stuff

--- config.el
+++ config.el
@@ -415,22 +415,6 @@
   (setq elfeed-search-title-max-width 120
         elfeed-search-filter "@1-week-ago--1-day-ago -youtube"))
 
-(use-package! elfeed-tube
-  ;; :straight (:host github :repo "karthink/elfeed-tube")
-  :after elfeed
-  :demand t
-  :config
-  ;; (setq elfeed-tube-auto-save-p nil) ;; t is auto-save (not default)
-  ;; (setq elfeed-tube-auto-fetch-p t) ;;  t is auto-fetch (default)
-  (elfeed-tube-setup)
-
-  :bind (:map elfeed-show-mode-map
-         ("F" . elfeed-tube-fetch)
-         ([remap save-buffer] . elfeed-tube-save)
-         :map elfeed-search-mode-map
-         ("F" . elfeed-tube-fetch)
-         ([remap save-buffer] . elfeed-tube-save)))
-
 
 ;;
 ;;; Language customizations
