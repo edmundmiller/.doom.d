eea30d4efa1f80d7c3becdeb5108e5b600e7ce5d
Edmund Miller <edmund.a.miller@protonmail.com>
2023-11-22 10:51:07 -0600

    feat: Add ob-duckdb

--- config.el
+++ config.el
@@ -493,6 +493,7 @@
 (use-package! nextflow-mode
   :config
   (set-docsets! 'nextflow-mode "Groovy"))
+(use-package! ob-duckdb)
 (use-package! ox-chameleon
   :after ox)
 (use-package! snakemake-mode)
