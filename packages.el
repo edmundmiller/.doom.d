a500ef71298a89eb60bfe41a264acb9c6c9b6984
Edmund Miller <git@edmundmiller.dev>
2024-04-27 21:44:12 -0500

    feat: Remove lsp-bridge

--- packages.el
+++ packages.el
@@ -51,15 +51,6 @@
 
 (package! elfeed-tube)
 (package! elfeed-tube-mpv)
-(when (package! lsp-bridge
-        :recipe (:host github
-                 :repo "manateelazycat/lsp-bridge"
-                 :branch "master"
-                 :files ("*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
-                 ;; do not perform byte compilation or native compilation for lsp-bridge
-                 :build (:not compile)))
-  (package! markdown-mode)
-  (package! yasnippet))
 
 (provide 'packages)
 ;;; packages.el ends here
