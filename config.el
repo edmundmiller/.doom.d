4d0c10fb1235942571e07d054f68169937c40884
Edmund Miller <git@edmundmiller.dev>
2024-08-29 13:55:48 -0500

    fix: Remove obsidian

--- config.el
+++ config.el
@@ -525,31 +525,6 @@
 
 ;;
 ;;; Packages
-(use-package! obsidian
-  :config
-  (obsidian-specify-path "~/archive/milldocs-2")
-  (global-obsidian-mode t)
-  :custom
-  ;; This directory will be used for `obsidian-capture' if set.
-  (obsidian-inbox-directory "Inbox")
-  ;; Create missing files in inbox? - when clicking on a wiki link
-  ;; t: in inbox, nil: next to the file with the link
-  ;; default: t
-                                        ;(obsidian-wiki-link-create-file-in-inbox nil)
-  ;; The directory for daily notes (file name is YYYY-MM-DD.md)
-  (obsidian-daily-notes-directory "Daily Notes")
-  ;; Directory of note templates, unset (nil) by default
-                                        ;(obsidian-templates-directory "Templates")
-  ;; Daily Note template name - requires a template directory. Default: Daily Note Template.md
-                                        ;(obsidian-daily-note-template "Daily Note Template.md")
-  :bind (:map obsidian-mode-map
-              ;; Replace C-c C-o with Obsidian.el's implementation. It's ok to use another key binding.
-              ("C-c C-o" . obsidian-follow-link-at-point)
-              ;; Jump to backlinks
-              ("C-c C-b" . obsidian-backlink-jump)
-              ;; If you prefer you can use `obsidian-insert-link'
-              ("C-c C-l" . obsidian-insert-wikilink)))
-
 (use-package! org-nix-shell
   :hook (org-mode . org-nix-shell-mode))
 
