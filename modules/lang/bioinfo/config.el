eff7c8298d2d88694381f36261cbdadb609f274b
Edmund Miller <git@edmundmiller.dev>
2024-08-29 13:58:50 -0500

    feat: First stab at nf-lsp

--- modules/lang/bioinfo/config.el
+++ modules/lang/bioinfo/config.el
@@ -2,7 +2,12 @@
 
 (use-package! nextflow-mode
   :config
-  (set-docsets! 'nextflow-mode "Groovy"))
+  (set-docsets! 'nextflow-mode "Groovy")
+  (add-to-list 'lsp-language-id-configuration '(nextflow-mode . "nextflow"))
+  (lsp-register-client (make-lsp-client
+                        :new-connection (lsp-stdio-connection '("java" "-jar" "~/src/nf-core/language-server/build/libs/language-server-all.jar"))
+                        :activation-fn (lsp-activate-on "nextflow")
+                        :server-id 'nf-ls)))
 
 (use-package! snakemake-mode
   :config
