8b763e2e2c91d851ab3d334f6ea48f70218d07c9
Edmund Miller <git@edmundmiller.dev>
2024-11-04 18:07:05 +0100

    feat: Update for lsp working

--- modules/lang/bioinfo/config.el
+++ modules/lang/bioinfo/config.el
@@ -2,12 +2,7 @@
 
 (use-package! nextflow-mode
   :config
-  (set-docsets! 'nextflow-mode "Groovy")
-  (add-to-list 'lsp-language-id-configuration '(nextflow-mode . "nextflow"))
-  (lsp-register-client (make-lsp-client
-                        :new-connection (lsp-stdio-connection '("java" "-jar" "~/src/nf-core/language-server/build/libs/language-server-all.jar"))
-                        :activation-fn (lsp-activate-on "nextflow")
-                        :server-id 'nf-ls)))
+  (setq-hook! 'nextflow-mode +format-with 'lsp))
 
 (use-package! snakemake-mode
   :config
--- modules/lang/bioinfo/packages.el
+++ modules/lang/bioinfo/packages.el
@@ -2,4 +2,5 @@
 ;;; lang/bioinfo/packages.el
 
 (package! nextflow-mode :recipe (:host github :repo "edmundmiller/nextflow-mode"))
+(package! lsp-mode :recipe (:local-repo "~/src/emacs/lsp-mode" :build (:not compile)))
 (package! snakemake-mode)
