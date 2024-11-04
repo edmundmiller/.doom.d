b5ce985a5bc81629ce53b46fc950a6b47d807d24
Edmund Miller <git@edmundmiller.dev>
2024-11-04 20:04:25 +0100

    feat: I said I'd follow the Harshil Alignment™️
    When there was a formatter that would make it consistent for me.

--- modules/lang/nextflow/config.el
+++ modules/lang/nextflow/config.el
@@ -6,4 +6,5 @@
     (add-hook 'nextflow-mode-hook #'lsp! 'append))
   :config
   (when (modulep! +lsp)
-    (setq-hook! 'nextflow-mode +format-with 'lsp)))
+    (setq-hook! 'nextflow-mode +format-with 'lsp)
+    (setq! lsp-nextflow-formatting-harshil-alignment t)))
