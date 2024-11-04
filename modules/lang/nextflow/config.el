772b0afa566c0005938c7e4250c10c883ececbe0
Edmund Miller <git@edmundmiller.dev>
2024-11-04 20:03:09 +0100

    fix(nextflow): Add lsp hook

--- modules/lang/nextflow/config.el
+++ modules/lang/nextflow/config.el
@@ -1,5 +1,9 @@
 ;;; lang/nextflow/config.el -*- lexical-binding: t; -*-
 
 (use-package! nextflow-mode
+  :init
+  (when (modulep! +lsp)
+    (add-hook 'nextflow-mode-hook #'lsp! 'append))
   :config
-  (setq-hook! 'nextflow-mode +format-with 'lsp))
+  (when (modulep! +lsp)
+    (setq-hook! 'nextflow-mode +format-with 'lsp)))
